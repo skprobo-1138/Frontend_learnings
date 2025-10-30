62c632f62106eb62b34d02973b8cd6c3e0f5c25c
Deep Shah <shahdeep966@gmail.com>
2025-10-30 14:19:50 +0530

    Components

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,13 +1,19 @@
 
 
 import React from 'react'
-
+import Card from './components/card.jsx'
+import NavBar from './components/NavBar.jsx'
 const App = () => {
   return (
     <>
-      <div id="skpro">dadasd</div>
-      <div id="dadasdi"></div>
+      <div>
+        <Card />
+        <NavBar />
+        <NavBar />
+      </div>
+      
     </>
+    
   )
 }
 
---
+++ 01-folder/src/components/card.jsx
@@ -0,0 +1,17 @@
+function Card()
+{
+    return (
+      <div className="car">
+        <h1>Shubham Patel</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quisquam
+          perferendis cumque numquam quas, necessitatibus, obcaecati expedita
+          suscipit corrupti tempora unde neque temporibus. Velit esse eos
+          eveniet ducimus, laboriosam magni.
+        </p>
+      </div>
+    );
+ 
+}
+
+export default Card
