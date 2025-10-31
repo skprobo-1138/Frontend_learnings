031388598cbeb1f8f6598e89b80ba81aa5618be6
Shubham Patel <skprobo1138@gmail.com>
2025-10-31 19:11:37 +0530

    commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import ParticlesBG from "./Components/ParticlesBG.jsx";
+import Hero from "./Components/Hero.jsx";
+
+export default function App( ) {
+  return  ( 
+    <div className="relative">
+      <ParticlesBG />
+      <Hero /> 
+    </div>
+  );
+}
